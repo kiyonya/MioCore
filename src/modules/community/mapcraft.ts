b35be2b986b37cbfd3a5d661687851c91281268d
kiyonya <481366085@qq.com>
2025-11-04 13:36:15 +0800

    fixed:change dir

---
+++ src/modules/community/mapcraft.ts
@@ -0,0 +1,12 @@
+export interface MapCraftSearchMapOptions {
+
+}
+
+export type MapCates = "parkour-maps" | 'puzzle-maps' | 'adventure-maps' | 'finding-maps' | 'horror-maps' | 'game-maps' | 'survival-maps' | 'pvp-maps' | 'creation-maps' | 'ctm-maps' | 'pve-maps' | 'escape-maps' | 'maze-maps' | 'dropper-maps' | 'hourse-maps' | 'city-maps' | 'trivia-maps' | 'variety-maps' | 'castle-maps' | 'unfair-maps' | 'custom-commands' | 'hide-and-seek-maps'
+
+export default class MapCraftMe {
+
+    public static async searchMap() {
+
+    }
+}
